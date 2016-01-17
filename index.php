fdef3581586f1dae6306af5dc718a538dbfdfa9b
Roldhan Dasalla <admin@roldhandasalla.com.ph>
2016-01-17 14:03:23 +0800

    adding files

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Fisher Valley College</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name=description content="The Fisher Valley College" />
+	<meta name=author content="Roldhan Dasalla(iNew Works)" />
+	<meta property=og:url content=http://www.tfvc-gradingsystem.com/ />
+	<meta property=og:type content=website />
+	<meta property=og:title content="The Fisher Valley College" />
+	<meta property=og:image content="" />
+	<meta property=og:description content="TFVC Student Grading System" />
+	<meta property=profile:first_name content="Roldhan Dasalla(iNew Works)" />
+
+	<link rel="stylesheet" href="assets/css/bootstrap.css"/>
+	<link rel="stylesheet" href="assets/css/index.css"/>
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" style="color:#eee">
+        <span class="sr-only" style="color:#eee">Toggle navigation</span>
+        <span class="icon-bar" style="color:#eee"></span>
+        <span class="icon-bar" style="color:#eee"></span>
+        <span class="icon-bar" style="color:#eee"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="color:#eee">TFVC</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Academics</a></li>
+        <li><a href="/gradingsystem/">Grading System</a></li>
+        <li class="dropdown" style="display:none">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-defaul">
+				<div class="panel-body" style="padding:0px">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+					  </ol>
+
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner" role="listbox">
+					    <div class="item active">
+					      <img src="./assets/img/a1.jpg" alt="..." width="100%" height="250px">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/img/a2.jpg" alt="..." width="100%" height="250px">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/img/a3.jpg" alt="..." width="100%" height="250px">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/img/a4.jpg" alt="..." width="100%" height="250px">
+					    </div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<img src="./assets/img/1.png" width="100%" class="img-responsive">
+		</div>
+		<div class="col-md-3">
+			<img src="./assets/img/2.png" width="100%">
+		</div>
+		<div class="col-md-3">
+			<img src="./assets/img/3.png" width="100%">
+		</div>
+		<div class="col-md-3">
+			<img src="./assets/img/4.png" width="100%">
+		</div>
+	</div>
+</div>
+<!-- S C R I P T S -->
+<script src="assets/js/bootstrap.js" type="text/javascript"></script>
+<script src="assets/js/inew.compiled.js" type="text/javascript"></script>
+</body>
+</html>
